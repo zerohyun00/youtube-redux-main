de5c516fa9a5546a7599eae129d667e2fdeb3129
DESKTOP-ATQDFAK\kyhh3 <kyhh39@naver.com>
2023-02-09 10:27:23 +0900

    youtube

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 
-const API_KEY=`AIzaSyAJb1fuxCzDpGVavFcOUYWqh2bOBALhWz8`;
+const API_KEY=process.env.REACT_APP_API_KEY;
 
 export const videoUrl=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
 
